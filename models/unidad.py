46d55f3eab0f5d15c5609cdeaedf3cd764b77efd
Adercito69 <ulicarlo.2109@gmail.com>
2026-06-27 21:18:09 -0600

    Se agrego el modulo de unidades

---
+++ models/unidad.py
@@ -0,0 +1,10 @@
+class Unidad:
+    def __init__(self, id=None, noeconomico=None, placas=None, modelo=None, marca=None, anio=None, kilometraje=None, estatus=None):
+        self.id = id
+        self.noeconomico = noeconomico
+        self.placas = placas
+        self.modelo = modelo
+        self.marca = marca
+        self.anio = anio
+        self.kilometraje = kilometraje
+        self.estatus = estatus
