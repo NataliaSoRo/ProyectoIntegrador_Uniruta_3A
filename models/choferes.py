e68cbe567b9b047589bb6ab60ea690a6bbac3726
NataliaSoRo <sosarodrigueznatalia451@gmail.com>
2026-07-11 14:05:47 -0600

    Commit creación choferes.py

---
+++ models/choferes.py
@@ -0,0 +1,9 @@
+class Choferes:
+    def __init__(self, id=None, nombre=None, telefono=None, licencia=None, tipo_licencia=None, vigen_licencia=None,  estatus=None):
+        self.id = id
+        self.nombre = nombre
+        self.telefono = telefono
+        self.licencia = licencia
+        self.tipo_licencia = tipo_licencia
+        self.vigen_licencia = vigen_licencia
+        self.estatus = estatus
