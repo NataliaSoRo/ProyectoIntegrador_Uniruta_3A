64771216b8b81f060716c11a49c4e384989c2e59
Adercito69 <ulicarlo.2109@gmail.com>
2026-07-12 02:26:04 -0600

    Se agrego el modulo viajes y CRUD de unidades

---
+++ models/viaje.py
@@ -0,0 +1,10 @@
+class Viaje:
+
+    def __init__(self, viaj_id=None, viaj_origen=None, viaj_destino=None, viaj_fecha=None, viaj_hora=None, viaj_unid_id=None, viaj_estatus=None):
+        self.viaj_id = viaj_id
+        self.viaj_origen = viaj_origen
+        self.viaj_destino = viaj_destino
+        self.viaj_fecha = viaj_fecha
+        self.viaj_hora = viaj_hora
+        self.viaj_unid_id = viaj_unid_id
+        self.viaj_estatus = viaj_estatus
